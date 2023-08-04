357842e8d2c0b0f2874aff2c5faaacbcc19198fc
Ozodbek <tursunovozodbek505@gmail.com>
2023-08-04 15:35:44 +0500

    to do list application

---
+++ main.js
@@ -0,0 +1,42 @@
+// variables
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
+const inputTask = document.getElementById("input-task");
+
+//Event
+addTask.addEventListener("click", function () {
+  let task = document.createElement("div");
+  task.classList.add("task");
+
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = "<i class='bx bx-check' ></i>";
+  checkButton.classList.add("checkTask");
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "<i class='bx bxs-trash-alt' ></i>";
+  deleteButton.classList.add("deleteTask");
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === "") {
+    alert("Please enter a task!");
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+
+  checkButton.addEventListener("click", function () {
+    checkButton.parentElement.style.textDecoration = "line-through";
+  });
+
+  deleteButton.addEventListener("click", function (e) {
+    let target = e.target;
+
+    target.parentElement.parentElement.remove();
+  });
+});
